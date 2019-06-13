bdb696338001a8f714a6d3f67ee6479b8675d941
Johannes Wågen <johannes@wagen.no>
2019-06-14 00:50:39 +0200

    bitmap2component: Added support for multiple input dimentions. Added button for aspect ratio, not hooked up yet.

--- bitmap2component/bitmap2cmp_gui_base.h
+++ bitmap2component/bitmap2cmp_gui_base.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Dec  1 2018)
+// C++ code generated with wxFormBuilder (version Oct 26 2018)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
@@ -22,10 +22,12 @@
 #include <wx/icon.h>
 #include <wx/notebook.h>
 #include <wx/stattext.h>
+#include <wx/bmpbuttn.h>
+#include <wx/button.h>
 #include <wx/textctrl.h>
+#include <wx/choice.h>
 #include <wx/sizer.h>
 #include <wx/statbox.h>
-#include <wx/button.h>
 #include <wx/radiobox.h>
 #include <wx/slider.h>
 #include <wx/checkbox.h>
@@ -53,17 +55,21 @@ class BM2CMP_FRAME_BASE : public KIWAY_PLAYER
 		wxStaticText* m_SizeXValue;
 		wxStaticText* m_SizeYValue;
 		wxStaticText* m_SizePixUnits;
-		wxStaticText* m_staticTextSize1;
-		wxStaticText* m_SizeXValue_mm;
-		wxStaticText* m_SizeYValue_mm;
-		wxStaticText* m_Size_mmxUnits;
+		wxStaticText* m_staticTextDPI;
+		wxStaticText* m_InputXValueDPI;
+		wxStaticText* m_InputYValueDPI;
+		wxStaticText* m_DPIUnit;
 		wxStaticText* m_staticTextBPP;
 		wxStaticText* m_BPPValue;
 		wxStaticText* m_BPPunits;
+		wxStaticText* m_BPPunits1;
+		wxStaticText* m_BPPunits2;
+		wxBitmapButton* m_AspectRatioLockButton;
+		wxStaticText* m_BPPunits3;
 		wxStaticText* m_staticTextBPI;
-		wxTextCtrl* m_DPIValueX;
-		wxTextCtrl* m_DPIValueY;
-		wxStaticText* m_DPI_Units;
+		wxTextCtrl* m_UnitSizeX;
+		wxTextCtrl* m_UnitSizeY;
+		wxChoice* m_PixelUnit;
 		wxButton* m_buttonLoad;
 		wxButton* m_buttonExportFile;
 		wxButton* m_buttonExportClipboard;
@@ -78,9 +84,10 @@ class BM2CMP_FRAME_BASE : public KIWAY_PLAYER
 		virtual void OnPaintInit( wxPaintEvent& event ) { event.Skip(); }
 		virtual void OnPaintGreyscale( wxPaintEvent& event ) { event.Skip(); }
 		virtual void OnPaintBW( wxPaintEvent& event ) { event.Skip(); }
-		virtual void UpdatePPITextValueX( wxMouseEvent& event ) { event.Skip(); }
+		virtual void ToggleAspectRatioLock( wxCommandEvent& event ) { event.Skip(); }
+		virtual void UpdateUnitTextValueX( wxMouseEvent& event ) { event.Skip(); }
 		virtual void OnResolutionChange( wxCommandEvent& event ) { event.Skip(); }
-		virtual void UpdatePPITextValueY( wxMouseEvent& event ) { event.Skip(); }
+		virtual void UpdateUnitTextValueY( wxMouseEvent& event ) { event.Skip(); }
 		virtual void OnLoadFile( wxCommandEvent& event ) { event.Skip(); }
 		virtual void OnExportToFile( wxCommandEvent& event ) { event.Skip(); }
 		virtual void OnExportToClipboard( wxCommandEvent& event ) { event.Skip(); }
@@ -91,7 +98,7 @@ class BM2CMP_FRAME_BASE : public KIWAY_PLAYER
 
 	public:
 
-		BM2CMP_FRAME_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Bitmap to Component Converter"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 733,616 ), long style = wxDEFAULT_FRAME_STYLE|wxRESIZE_BORDER|wxTAB_TRAVERSAL );
+		BM2CMP_FRAME_BASE( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Bitmap to Component Converter"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 746,616 ), long style = wxDEFAULT_FRAME_STYLE|wxRESIZE_BORDER|wxTAB_TRAVERSAL );
 
 		~BM2CMP_FRAME_BASE();
 
